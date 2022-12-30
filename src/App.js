a44c81eefd5e1e2bc0b85051d3cf93089ec923b6
Abhimanyu Debata <107947041+abhimanyu6542@users.noreply.github.com>
2022-12-30 23:34:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Board from './components/Board';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="header">TIC TAC TOE</h1>
+      <Board />
+
+      <h1 className="header">Developed By Abhimanyu Debata</h1>
+
+    </div>
+  );
+}
+
+export default App;
